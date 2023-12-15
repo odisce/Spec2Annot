b2b0da4aa5c060e9a20c1e5d177ff6b0b3b98e20
Sylvain Dechaumet <sylvain.dechaumet@gmail.com>
2023-12-15 18:49:22 +0100

    Update data.R
    added @export tags to datas

--- R/data.R
+++ R/data.R
@@ -1,33 +1,33 @@
 #' Periodic table as a data.table
 #'
 #' @references \url{https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl?ele=&ascii=ascii2&isotype=some}
-#' 
+#' @export
 "Element"
 
 #' Mono charged ions
-#' 
+#' @export
 "Monocharge_db"
 
 #' adduct data
-#' 
+#' @export
 "Adduct_db"
 
 #' isotopes data
-#' 
+#' @export
 "Isotopes_db"
 
 #' losses data
-#' 
+#' @export
 "Losses_db"
 
 #' ions data
-#' 
+#' @export
 "ions_database"
 
 #' Spectra exemple
-#' 
+#' @export
 "spectra_full"
 
 #' Valence data
-#' 
+#' @export
 "valence_db"
