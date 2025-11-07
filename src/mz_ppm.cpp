3a5f909adc31b0592125b39f4cc48a60b8c86239
Sylvain Dechaumet <29856567+sdechaumet@users.noreply.github.com>
2025-11-07 11:28:57 +0100

    Update mz_ppm.cpp
    use mass +- ppm(mass) instead of mass +- ppm(mass)/2

--- src/mz_ppm.cpp
+++ src/mz_ppm.cpp
@@ -25,7 +25,7 @@ double mz_ppm(
 //' Get mass range with ppm
 //'
 //' @param mass First mass
-//' @param ppm ppm tolerance
+//' @param ppm ppm tolerance mass +- ppm(mass)
 //' @export
 //'
 // [[Rcpp::export]]
@@ -33,7 +33,7 @@ Rcpp::NumericVector mz_range(
   double mass = 120.1253,
   double ppm = 10
 ) {  
-  double mzdiff = (ppm * mass / 1e6 / 2);
+  double mzdiff = (ppm * mass / 1e6);
   Rcpp::NumericVector output = {mass - mzdiff, mass + mzdiff};
   return output;
 }
