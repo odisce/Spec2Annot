4474708f150a7d78ea7d50360d7c74de74f308a6
sdechaumet <sylvain.dechaumet@cea.fr>
2026-07-15 10:14:08 +0000

    flaged test of function in development

--- tests/testthat/test-annotate_spectra.R
+++ tests/testthat/test-annotate_spectra.R
@@ -1,11 +1,15 @@
+testthat::skip("Dev: in progress")
+require(magrittr)
+
 test_that(
   "Spec2Annot::annotat_spectra", {
     spectra_in <- Spec2Annot::spectra_full
     ## Create diff grid
     # Rcpp::sourceCpp("./src/cpp_annotate_spectra.cpp")
-    res <- annotate_spectra(spectra_in[sample(seq_len(.N), 10), mz] %>% sort())
-    find_comb(10, 2)
-    
+    for (i in seq_len(100)) {
+      res <- annotate_spectra(spectra_in[sample(seq_len(.N), 10), mz] %>% sort())
+      temp <- find_comb(10, 2)
+    }
     # devtools::load_all()
     # combn_cpp(spectra_in[, unique(mz)][1:10], 2)
   }
